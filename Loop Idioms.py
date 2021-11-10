55c5cc5c0cbfac336c3083e9b8d30ae0898d41d0
Mayaka786 <angwenyimayaka@gmail.com>
2021-11-10 14:45:50 -0800

    Summing up loops

---
+++ Loop Idioms.py
@@ -0,0 +1,11 @@
+zork=0
+print('Before',zork)
+for thing in [6,8,2,4,6,5,2,3,7]:
+    zork=zork+ thing
+    print(zork,thing)
+print('After',zork)
+
+
+
+
+
---
+++ Summing in a loop.py
@@ -0,0 +1,7 @@
+zork=0
+print('Before',zork)
+for thing in[6,8,7,4,9,5]:
+    zork=zork+1
+    print(zork,thing)
+print('After',zork)
+    
