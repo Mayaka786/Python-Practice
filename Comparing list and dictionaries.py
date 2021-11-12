8e40a8973cb636e52ab844183323ec02330e44db
Mayaka786 <angwenyimayaka@gmail.com>
2021-11-12 15:43:52 -0800

    Comparing lists and dictionaries

---
+++ Comparing list and dictionaries.py
@@ -0,0 +1,6 @@
+lst=list()
+lst.append(21)
+lst.append(215)
+print(lst)
+lst[1]=78
+print(lst)
---
+++ email split.py
@@ -0,0 +1,8 @@
+purse=dict()
+purse['money']=12
+purse['Candy']=8
+purse['Tissue']=45
+print(purse)
+
+purse['candy']=purse['candy']+2
+print('purse')
