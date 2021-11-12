a19f85f3ca05a311b53cbc7babd9a9d82fcabff8
Mayaka786 <angwenyimayaka@gmail.com>
2021-11-12 14:57:40 -0800

    Split functions

---
+++ Split.py
@@ -0,0 +1,8 @@
+abc='A lot'
+etc=abc.split()
+print(etc)
+
+
+nice='It has been a while'
+lol=nice.split()
+print(lol)
--- split function.py
+++ split function.py
@@ -1,7 +1,1 @@
-abc="With three words"
-piece=abc.split()
-print(piece)
 
-new="Split me into five words"
-word=new.split()
-print(word)
