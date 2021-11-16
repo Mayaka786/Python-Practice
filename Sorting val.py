42fe563eb7f4f103b5ea1006376b252fc2331c07
Mayaka786 <angwenyimayaka@gmail.com>
2021-11-16 10:31:32 -0800

    Sorting val

---
+++ Sorting val.py
@@ -0,0 +1,14 @@
+fhand=open('ian.txt')
+counts=dict()
+for line in fhand:
+    words=line.split
+    for word in words:
+        counts[word]=counts.get(word,0) + 1
+lst=list()
+for key,val in counts.items():
+    newtup =(val, key)
+    lst.append(newtup)
+    
+lst=sorted(lst,reverse=True)
+for val,key in lst[:10]:
+    print(key,val)
