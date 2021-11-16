0e75962c10a4f0cb4233d00a6d513b6da4b0635c
Mayaka786 <angwenyimayaka@gmail.com>
2021-11-16 09:29:36 -0800

    Sorting values

---
+++ Sorting values.py
@@ -0,0 +1,5 @@
+p={'d':1,'e':5,'f':7}
+tmp=list()
+for k,v in p.items():
+    tmp.append((v,k))
+print(tmp)
---
+++ o.py
@@ -0,0 +1,4 @@
+d={'a':10,'b:24':,'c':2}
+d.items
+dict_items({'a':10,'c':2,'b:24':})
+sorted(d.items())
